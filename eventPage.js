a8199369803f3fc3e7e23aaf051262cb10855bf7
givemin5 <givemin5@gmail.com>
2017-09-13 00:18:58 +0800

    Chrome Extension - Context Menu Functionality Completed

--- eventPage.js
+++ eventPage.js
@@ -1,7 +1,46 @@
 var contextMenuItem = {
-    "id" : "spendMoney",
-    "title" : "SpendMoney",
-    "contexts" : ["selection"]
+    "id": "spendMoney",
+    "title": "SpendMoney",
+    "contexts": ["selection"]
 };
 
 chrome.contextMenus.create(contextMenuItem);
+
+chrome.contextMenus.onClicked.addListener(function (clickData) {
+    if (clickData.menuItemId == "spendMoney" && clickData.selectionText) {
+        if (isInt(clickData.selectionText)) {
+            var amount = clickData.selectionText;
+
+            chrome.storage.sync.get(['total', 'limit'], function (budget) {
+                var newTotal = 0;
+                if (budget.total) {
+                    newTotal += parseInt(budget.total);
+                }
+                newTotal += parseInt(amount);
+                chrome.storage.sync.set({'total' : newTotal},function(){
+                if(newTotal >= budget.limit){
+                    var notifOptions = {
+                        type : 'basic',
+                        iconUrl : 'icon128.png',
+                        title : 'Limit reached!',
+                        message : "Uh oh! Looks you've reached your limnit!"
+                    };
+                    chrome.notifications.create('limitNotif',notifOptions);
+                }
+
+            });
+
+
+            })
+
+        }
+
+    }
+
+})
+
+function isInt(value) {
+    return !isNaN(value) &&
+        parseInt(Number(value)) == value &&
+        !isNaN(parseInt(value, 10));
+}
